e917010a23fc79b330c36fc3be912217d52867f8
Sydney-Taylor-Monteiro <155786776+Sydney-Taylor-Monteiro@users.noreply.github.com>
2024-01-21 23:39:41 +0200

    updated js

--- src/index.js
+++ src/index.js
@@ -1,1 +1,7 @@
-alert("Hello World");
+function search(event); {
+    event.preventDefault();
+    alert("searching");
+}
+
+let searchForm = document.querySelector("search-form");
+searchForm.addEventListener("submit", search);
