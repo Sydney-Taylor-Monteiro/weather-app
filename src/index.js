64ebe6f6bb367451bc9ea7ace6feb928e8719792
Sydney-Taylor-Monteiro <155786776+Sydney-Taylor-Monteiro@users.noreply.github.com>
2024-01-22 01:51:31 +0200

    Updated js

--- src/index.js
+++ src/index.js
@@ -4,3 +4,39 @@ function search(event) {
   let cityElement = document.querySelector("#current-city");
   cityElement.innerHTML = searchInputElement.value;
 }
+
+function formattedDate(date) {
+  let minutes = currentDate.getMinutes();
+  let hours = currentDate.getHours();
+  let day = currentDate.getDay();
+
+  if (minutes < 10) {
+    minutes = `0${minutes}`;
+  }
+
+  if (hours < 10) {
+    hours = `0${hours}`;
+  }
+
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+
+  let formattedDay = days[day];
+
+  return `${formattedDay} ${hours}:${minutes}`;
+}
+
+let searchForm = document.querySelector("search-form");
+searchForm.addEventListener("submit", search);
+
+let currentDateElement = document.querySelector("current-date");
+let currentDate = new Date();
+
+currentDateElement.innerHTML = formatDate(currentDate);
