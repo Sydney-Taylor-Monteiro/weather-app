e69938e0f5ed564904d4aadb6d46f7d9f2f316c3
Sydney-Taylor-Monteiro <155786776+Sydney-Taylor-Monteiro@users.noreply.github.com>
2024-01-22 00:22:11 +0200

    updated js

--- src/index.js
+++ src/index.js
@@ -1,8 +1,6 @@
 function search(event) {
   event.preventDefault();
-  let searchInputElement = document.querySelector("#search-input");
   console.log(searchInputElement);
-  alert(searchInputElement.value);
 }
 
 let searchForm = document.querySelector("search-form");
