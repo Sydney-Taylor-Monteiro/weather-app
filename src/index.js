729ad0150df977dababc78bc71de335d839963aa
Sydney-Taylor-Monteiro <155786776+Sydney-Taylor-Monteiro@users.noreply.github.com>
2024-01-22 01:03:23 +0200

    updated js and html

---
+++ src/index.js
@@ -0,0 +1,14 @@
+function search(event) {
+    event.preventDefault();
+    let searchInputElement = document.querySelector("#search-input");
+    let cityElement = document.querySelector("#current-city");
+    cityElement.innerHTML = searchInputElement.value;
+  }
+  
+  function formatDate(date) {
+    let minutes = date.getMinutes();
+    let hours = date.getHours();
+    let day = date.getDay();
+  
+    if (minutes < 10) {
+      minutes = `0${minutes}`;
