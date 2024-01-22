8cbff49b27dfe42fec4ac777e16a662bd1456843
Sydney-Taylor-Monteiro <155786776+Sydney-Taylor-Monteiro@users.noreply.github.com>
2024-01-22 02:22:54 +0200

    added apikey and apiurl in html

--- src/index.js
+++ src/index.js
@@ -2,7 +2,10 @@ function search(event) {
   event.preventDefault();
   let searchInputElement = document.querySelector("#search-input");
   let cityElement = document.querySelector("#current-city");
-  cityElement.innerHTML = searchInputElement.value;
+  let city = searchInputElement.value;
+  let apiKey = "3bd321a8a0a23a8bo4488e4fce17e2tf";
+  let apiURL =
+    "https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric";
 }
 
 function formatDate(date) {
